ad1851409e67a131a576ba9e0ab039cb13677333
“Isaque <isaquefabro04@gmail.com>
2025-06-16 10:31:35 -0300

    database connection fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
 
 import java.sql.Connection;
+import org.example.util.DatabaseConnection;
 
 public class Main {
     public static void main(String[] args) {
         try (Connection conn = DatabaseConnection.getConnection()) {
             System.out.println("Conectado com sucesso!");
-            //  DAOImpls
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/org/example/util/DatabaseConnection.java
+++ src/main/java/org/example/util/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/teste_java";
     private static final String USER = "isaquefabro";
-    private static final String PASSWORD = ""; // se houver senha, adicione aqui
+    private static final String PASSWORD = ""; // se tiver*
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASSWORD);
